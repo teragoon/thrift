d0c2aeb6d368d3059606b2b1e6e38d104ad920bd
teragoon <teragoon@naver.com>
2012-03-29 02:31:57 +0000

    maven build 옵션 추가

---
+++ src/main/java/simple/Thrift.java
@@ -0,0 +1,35 @@
+package simple;
+
+import org.apache.thrift.server.THsHaServer;
+import org.apache.thrift.server.THsHaServer.Args;
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.transport.TNonblockingServerSocket;
+import org.apache.thrift.transport.TNonblockingServerTransport;
+import org.apache.thrift.transport.TTransportException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import simple.PostService.Iface;
+import simple.PostService.Processor;
+
+public class Thrift {
+    	
+	ppublic static void main(String[] args) {
+		ApplicationContext context = new AnnotationConfigApplicationContext(
+				Config.class);
+				
+		try {
+			PostService.Iface handler  = (PostService.Iface) context.getBean("postHandler");
+			PostService.Processor<PostService.Iface> processor = new Processor<Iface>(handler);
+			
+			TNonblockingServerTransport serverTransport = new TNonblockingServerSocket(8081);
+			TServer server = new THsHaServer(new Args(serverTransport).workerThreads(10).processor(processor));
+			System.out.println("Server is ready!!!");
+			server.serve();
+		} catch (TTransportException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
